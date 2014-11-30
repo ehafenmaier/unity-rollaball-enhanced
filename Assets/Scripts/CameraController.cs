75cb81bffa4d8193b460e705746e429a4f75f88a
Erich Hafenmaier <ehafenmaier@Erichs-MacBook-Pro.local>
2014-11-30 13:29:51 -0700

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	#region Public Variables
+	public GameObject player;
+	#endregion
+
+	#region Private Variables
+	private Vector3 offset;
+	#endregion
+
+	// Use this for initialization
+	void Start() 
+	{
+		offset = transform.position;
+	}
+	
+	// LateUpdate is called last during the update cycle
+	void LateUpdate()
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	[Range(100.0f, 800.0f)]
+	public float speed;
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		// Create movement vector from input
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		// Add force to the player object based on movement vector
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
+
+	#region Trigger Collider Methods
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.tag == "PickUp")
+		{
+			other.gameObject.SetActive(false);
+		}
+	}
+	#endregion
+}
