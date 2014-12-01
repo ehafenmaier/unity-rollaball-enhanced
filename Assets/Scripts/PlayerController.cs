12cd58b36ad05d2f937cf7a4a0a7674e170ff283
Erich Hafenmaier <ehafenmaier@users.noreply.github.com>
2014-12-01 00:31:15 -0700

    Add score and UI
    Added the variables and methods for keeping trak of the score and displaying it with the new UI system.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,35 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour 
 {
+	#region Public Variables
+	// Using the Range attribute to set the minimum and maximum the 
+	// speed variable can be set to. This attribute will create a
+	// slider in the inspector.
 	[Range(100.0f, 800.0f)]
 	public float speed;
+	// Reference to the UI Text object that will display the score
+	public Text scoreText;
+	#endregion
+
+	#region Private Variables
+	// Variable to hold the score of the game (the number of PickUp
+	// objects collected)
+	private int score;
+	#endregion
+
+	void Start()
+	{
+		// Initialize the score to zero
+		score = 0;
+		UpdateScoreDisplay();
+	}
 
 	void FixedUpdate()
 	{
+		// Grab the values of the keyboard input (arrow keys)
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
 
@@ -21,10 +43,20 @@ public class PlayerController : MonoBehaviour
 	#region Trigger Collider Methods
 	void OnTriggerEnter(Collider other) 
 	{
+		// Check if the game object that the player object has 
+		// collided with is tagged "PickUp". If true, disable
+		// the game object and increase the score by one
 		if (other.gameObject.tag == "PickUp")
-		{
+		{	 
 			other.gameObject.SetActive(false);
+			score++;
+			UpdateScoreDisplay();
 		}
 	}
 	#endregion
+	
+	void UpdateScoreDisplay()
+	{
+		scoreText.text = string.Format("Score: {0}", score);
+	}
 }
